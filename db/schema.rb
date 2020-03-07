26cd6d1f30ce3293539b347602f9c1907bac45dc
Teerth Patel <teerth1205@gmail.com>
2020-03-07 03:22:11 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,4 +13,10 @@
 
 ActiveRecord::Schema.define(version: 20200307030909) do
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+  end
+
 end
